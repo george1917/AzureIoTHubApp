f4c28bc01ea9190daeb5153ebf49994084a79201
George <george@bud4.net>
2020-04-30 10:02:46 +0800

    init AzureIoTHubApp

---
+++ DeviceApp/Program.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.Azure.Devices.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DeviceApp
+{
+    //Microsoft.Azure.Devices.Client
+
+    class Program
+    {
+        private static string s_deviceConnectionString = "Device Connection String";
+        private static TransportType s_transportType = TransportType.Mqtt;
+        static CancellationTokenSource tokenSource = new CancellationTokenSource();
+        static readonly TaskScheduler _syncContextTaskScheduler = TaskScheduler.Current;
+        static int execCount = 0;
+
+        public static async Task Main(string[] args)
+        {
+            if (string.IsNullOrEmpty(s_deviceConnectionString) && args.Length > 0)
+            {
+                s_deviceConnectionString = args[0];
+            }
+
+            using (var deviceClient = DeviceClient.CreateFromConnectionString(s_deviceConnectionString, s_transportType))
+            {
+                _deviceClient = deviceClient;
+                await RunSampleAsync().ConfigureAwait(false);
+
+                GetWeight();
+                Console.Read();
+            }
+        }
+
+        #region Device get scale weight
+        private static void GetWeight()
+        {
+            CancellationToken token = tokenSource.Token;
+
+            Task task = Task.Factory.StartNew(() =>
+            {
+                while (!token.IsCancellationRequested)
+                {
+                    Random rnd = new Random();
+                    int a = rnd.Next(10000);
+                    decimal f = (decimal)(a * 0.01);
+
+                    Task.Factory.StartNew(() => pbBarFun(f), new CancellationTokenSource().Token, TaskCreationOptions.None, _syncContextTaskScheduler).Wait();
+
+                    Thread.Sleep(5000);
+                }
+            }, token);
+        } 
+         
+        private static void pbBarFun(decimal rFlag)
+        {
+            decimal preValue = rFlag - 1;
+            decimal nextValue = rFlag + 1;
+            CurrentWeight = rFlag;
+
+            execCount++;
+            Console.WriteLine("No. {0} :", execCount);
+            Console.WriteLine("  -> {0}", preValue);
+            Console.WriteLine("  -> {0}", nextValue);
+            Console.WriteLine("  -> {0}", rFlag);
+            Console.WriteLine("--------------------");
+        } 
+         
+        private static decimal? _CurrentWeight;
+        public static decimal? CurrentWeight
+        {
+            get { return _CurrentWeight; }
+            set
+            {
+                _CurrentWeight = value;
+            }
+        }
+        #endregion
+
+        #region Web call device method 
+        private static DeviceClient _deviceClient;
+        private class DeviceData
+        {
+            [JsonPropertyName("weight")]
+            public decimal? Weight { get; set; }
+        }
+
+        /// <summary>
+        /// Setup a callback for the 'GetWeight' method.
+        /// </summary> 
+        public static async Task RunSampleAsync()
+        {
+            DeviceData weight = null;
+            await _deviceClient
+                .SetMethodHandlerAsync("GetWeight", GetWeightAsync, weight)
+                .ConfigureAwait(false);
+        }
+
+        private static Task<MethodResponse> GetWeightAsync(MethodRequest methodRequest, object userContext)
+        {
+            Console.WriteLine($"\t *** {methodRequest.Name} was called.");
+
+            userContext = new DeviceData() { Weight = CurrentWeight };
+            string result = JsonSerializer.Serialize(userContext);
+            MethodResponse retValue = new MethodResponse(Encoding.UTF8.GetBytes(result), 200);
+
+            return Task.FromResult(retValue);
+        }
+        #endregion
+
+    }
+}
---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Azure.Devices;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using NTX.SysCore;
+using NTX.SysCore.Services;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp.Controllers
+{
+    //Microsoft.Azure.Devices
+
+    public class HomeController : Controller
+    {
+        private static ServiceClient s_serviceClient;
+        private readonly static string s_connectionString = "IoT Hub Connection String";
+
+        [HttpPost]
+        public async Task<string> GetWeightData()
+        {
+            s_serviceClient = ServiceClient.CreateFromConnectionString(s_connectionString);
+            string json = await InvokeMethod();
+            var deviceData = JsonSerializer.Deserialize<DeviceData>(json);
+            return string.Format(@"Weight：{0}", deviceData.Weight);
+        }
+
+        private class DeviceData
+        {
+            [JsonPropertyName("weight")]
+            public decimal? Weight { get; set; }
+        }
+
+        private async Task<string> InvokeMethod()
+        {
+            //Web call device method
+            var methodInvocation = new CloudToDeviceMethod("GetWeight") { ResponseTimeout = TimeSpan.FromSeconds(30) };
+            methodInvocation.SetPayloadJson("10");
+
+            // Invoke the direct method asynchronously and get the response from the simulated device.
+            var response = await s_serviceClient.InvokeDeviceMethodAsync("Device ID", methodInvocation);
+
+            return response.GetPayloadAsJson();
+        }
+
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+            return View();
+        }
+    }
+}
